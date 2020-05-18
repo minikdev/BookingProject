6c34ada76cc0c24c90687631a09c8954cf3044c2
minik <enginkaan6@gmail.com>
2020-05-19 00:06:48 +0300

    Finished authentication form.

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 import { Router } from "@angular/router";
-import { LoadingController } from '@ionic/angular';
+import { LoadingController } from "@ionic/angular";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-auth",
@@ -11,22 +12,44 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
-  constructor(private authService: AuthService, private router: Router, private loadingCtrl:LoadingController) {}
+  isLogin = true;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {}
-
 
   onLogin() {
     this.isLoading = true;
     this.authService.login();
-    this.loadingCtrl.create({keyboardClose:true,message:"Logging in..."}).then(loadingEl=>{
-      loadingEl.present();
-      setTimeout(()=>{
-        this.isLoading = false;
-        loadingEl.dismiss();
-        this.router.navigateByUrl('places/tabs/discover');
-      },1500);
-      
-    });
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: "Logging in..." })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl("places/tabs/discover");
+        }, 1500);
+      });
+  }
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+console.log(email,password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    }else{
+      // Send a request to signup servers
+    }
+  }
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
