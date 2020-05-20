e6b60479e57c144a9d1ef8ff2f019decc0ee8c65
minik <enginkaan6@gmail.com>
2020-05-21 00:19:57 +0300

    Created first firebase db, posted/fetched data

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -13,6 +13,7 @@ import { Subscription } from "rxjs";
 export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
   private placesSub: Subscription;
+  isLoading=false;
   constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
@@ -24,6 +25,13 @@ export class OffersPage implements OnInit, OnDestroy {
     slidingItem.close();
     this.router.navigate(["/", "places", "tabs", "offers", "edit", offerId]);
     console.log("Editing item", offerId);
+  }
+  
+  ionViewWillEnter(){
+    this.isLoading=true;
+    this.placesService.fetchPlaces().subscribe(()=>{
+      this.isLoading=false;
+    });
   }
 
   ngOnDestroy() {
