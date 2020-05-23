3b2874750503576f08337786d15cd02913a82b35
minik <enginkaan6@gmail.com>
2020-05-23 18:37:52 +0300

    Android and IOS apps builded and tested.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from "@angular/core";
-
+import { Plugins,Capacitor } from "@capacitor/core";
 import { Platform } from "@ionic/angular";
-import { SplashScreen } from "@ionic-native/splash-screen/ngx";
-import { StatusBar } from "@ionic-native/status-bar/ngx";
-import { AuthService } from './auth/auth.service';
-import { Router } from '@angular/router';
+import { AuthService } from "./auth/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
@@ -14,25 +12,24 @@ import { Router } from '@angular/router';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private authService:AuthService,
-    private router:Router
+
+    private authService: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
+    console.log(this.platform.is("hybrid"));
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+     if (Capacitor.isPluginAvailable("SplashScreen")) {
+       Plugins.SplashScreen.hide();
+       
+     }
     });
   }
   onLogout() {
     this.authService.logout();
-    this.router.navigateByUrl('/auth');
- 
+    this.router.navigateByUrl("/auth");
   }
-
-  
 }
